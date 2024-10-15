ba4bc5b8a36286faf8277f4d3bd5d6abc40198b1
Ant <129389119+bxrgman@users.noreply.github.com>
2024-10-15 18:39:08 -0400

    Updated Main.java with LinearRegression model

--- src/main/java/com/burgman/midterm/Main.java
+++ src/main/java/com/burgman/midterm/Main.java
@@ -1,16 +1,31 @@
 package com.burgman.midterm;
 
+import weka.classifiers.functions.LinearRegression;
 import weka.core.Instances;
 public class Main {
 
     public static void main(String[] args) {
         try {
+            // Initializes Data Preprocessing
             DataPreprocessing preprocessing = new DataPreprocessing();
 
+            // Load the dataset
             Instances data = preprocessing.loadDataset("data/train.csv");
+
+            // Handles any missing values
             data = preprocessing.handleMissingValues(data);
+
+            // Converts attributes to nominal (if necessary)
             data = preprocessing.convertToNominal(data);
-            System.out.println("Data Preprocessing is complete!");
+
+            // Model Training
+            ModelTrainer trainer = new ModelTrainer();
+
+            // Linear Regression Model here and evaluates
+            LinearRegression model = trainer.trainModel(data);
+            trainer.evaluateModel(model, data);
+
+            System.out.println("Model Training and Evaluation Complete!");
         } catch (Exception e) {
             e.printStackTrace();
         }
