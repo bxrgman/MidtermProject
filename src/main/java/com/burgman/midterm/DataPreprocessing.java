48c3a00e9f4d8d2c29e9640607b4b7db68806bbf
Ant <129389119+bxrgman@users.noreply.github.com>
2024-10-15 19:45:45 -0400

    Updated preprocessing to handle conversion without merging target

--- src/main/java/com/burgman/midterm/DataPreprocessing.java
+++ src/main/java/com/burgman/midterm/DataPreprocessing.java
@@ -15,6 +15,9 @@ public class DataPreprocessing {
         CSVLoader loader = new CSVLoader();
         loader.setSource(new File(filepath));
         Instances data = loader.getDataSet();
+
+        // Set the target attribute as the last attribute in the dataset
+        data.setClassIndex(data.numAttributes() - 1);
         return data;
     }
 
@@ -25,17 +28,22 @@ public class DataPreprocessing {
         return Filter.useFilter(data, replaceMissing);
     }
 
-    // Convert string attributes to nominal
+    // Convert string attributes to nominal (excluding the target attribute)
     public Instances convertStringToNominal(Instances data) throws Exception {
         StringToNominal stringToNominal = new StringToNominal();
-        stringToNominal.setAttributeRange("first-last"); // Apply to all string attributes
+
+        // Set range to convert all attributes except the last one (target)
+        stringToNominal.setAttributeRange("first-" + (data.numAttributes() - 2));
         stringToNominal.setInputFormat(data);
         return Filter.useFilter(data, stringToNominal);
     }
 
-    // Convert categorical attributes to nominal (if necessary)
+    // Convert numeric attributes that are categorical to nominal (excluding the target)
     public Instances convertToNominal(Instances data) throws Exception {
         NumericToNominal convert = new NumericToNominal();
+
+        // Convert only the relevant numeric attributes that are categorical (not target)
+        convert.setAttributeIndices("first-" + (data.numAttributes() - 2));
         convert.setInputFormat(data);
         return Filter.useFilter(data, convert);
     }
